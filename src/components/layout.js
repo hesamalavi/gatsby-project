1e888067d8525425658f0069f7cd4f0a73bd1bb7
Hesam Alavi <hesamalavi27@gmail.com>
2020-04-16 15:20:58 +1000

    Error correction for sass

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from "./header"
 import Footer from "./footer"
-import "../index.scss"
+import "../styles/index.scss"
 
 const Layout = props => {
   return (
