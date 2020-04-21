fc4221a8b5b2a842461764f63c8679e57bbc4113
Hesam Alavi <hesamalavi27@gmail.com>
2020-04-21 15:15:51 +1000

    Setup 404 page + render images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,39 @@
 module.exports = {
-  siteMetadata: {
-    title: "Hesam Alavi",
-    author: "Hesam Alavi",
-  },
-  plugins: [
-    {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-      }
-    }
-    "gatsby-plugin-sass",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "src",
-        path: `${__dirname}/src/`,
-      },
+    siteMetadata: {
+        title: "Hesam Alavi",
+        author: "Hesam Alavi",
     },
-    "gatsby-plugin-sharp",
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          "gatsby-remark-relative-images",
-          {
-            resolve: "gatsby-remark-images",
+    plugins: [
+        {
+            resolve: "gatsby-source-contentful",
             options: {
-              maxWidth: 750,
-              linkImagesToOriginal: false,
+                spaceId: process.env.CONTENTFUL_SPACE_ID,
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
             },
-          },
-        ],
-      },
-    },
-  ],
+        },
+        "gatsby-plugin-sass",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "src",
+                path: `${__dirname}/src/`,
+            },
+        },
+        "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-transformer-remark",
+            options: {
+                plugins: [
+                    "gatsby-remark-relative-images",
+                    {
+                        resolve: "gatsby-remark-images",
+                        options: {
+                            maxWidth: 750,
+                            linkImagesToOriginal: false,
+                        },
+                    },
+                ],
+            },
+        },
+    ],
 }
